888ed591c26046da3080bcbc57821a3c23c14276
chuxiaojie <1186982585@qq.com>
2025-04-29 20:22:55 +0800

    feat: test LLM

--- src/main/java/com/atguigu/java/ai/langchain4j/XiaozhiApp.java
+++ src/main/java/com/atguigu/java/ai/langchain4j/XiaozhiApp.java
@@ -2,6 +2,8 @@ package com.atguigu.java.ai.langchain4j;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
 /**
  * @Author ${user}
