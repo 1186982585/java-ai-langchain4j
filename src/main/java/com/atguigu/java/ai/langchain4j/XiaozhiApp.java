3b70e0971c1151a0d9cf98b830931d089614c6b4
chuxiaojie <1186982585@qq.com>
2025-04-29 19:55:29 +0800

    feat: init project

---
+++ src/main/java/com/atguigu/java/ai/langchain4j/XiaozhiApp.java
@@ -0,0 +1,16 @@
+package com.atguigu.java.ai.langchain4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author ${user}
+ * @Date 2025/4/28 17:35
+ * @Description:
+ */
+@SpringBootApplication
+public class XiaozhiApp {
+    public static void main(String[] args) {
+        SpringApplication.run(XiaozhiApp.class, args);
+    }
+}
